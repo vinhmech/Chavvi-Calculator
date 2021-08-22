d9dffa2464b4499c581bd13b5d5ca54592b13dec
vinhmech <vincenguyen262@gmail.com>
2021-08-21 21:04:47 -0700

    Initialize project

---
+++ src/chavvi_calculator/src/main/java/com/cis059/chavvi_calculator.java
@@ -0,0 +1,13 @@
+package com.cis059;
+
+/**
+ * Hello world!
+ *
+ */
+public class chavvi_calculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
