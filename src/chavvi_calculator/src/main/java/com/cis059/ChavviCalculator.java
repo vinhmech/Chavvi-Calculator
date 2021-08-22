fb6986bb897ff99d5e0603c9bf76466c0a617aa9
vinhmech <vincenguyen262@gmail.com>
2021-08-22 11:27:43 -0700

    create quit function & create loop for menu

--- src/chavvi_calculator/src/main/java/com/cis059/ChavviCalculator.java
+++ src/chavvi_calculator/src/main/java/com/cis059/ChavviCalculator.java
@@ -11,15 +11,37 @@ import java.util.Scanner;  // Import the Scanner class
  * @version 1.0 - 08/21/2021
  */
 
+
+
+
+
 public class ChavviCalculator 
 {
+    // function to check if input is float 
+    public static boolean isFloat(String value) {
+        int decimalPoint = 0;
+        if ((value.charAt(0) < '0' || value.charAt(0) > '9') && value.charAt(0) != '.' && value.charAt(0) != '-') return false;
+        if (value.charAt(0) == '.' && value.charAt(1) == '-') return false;
+        for (int i = 1; i < value.length(); i++) {
+            if ((value.charAt(i) < '0' || value.charAt(i) > '9') && value.charAt(i) != '.') {
+                return false;
+            }
+            if (value.charAt(i) == '.') {
+                decimalPoint++;
+                if (decimalPoint > 1) return false;
+            }
+        }
+        return true;
+    }
+
     // print menu function
     public static void menu(){
-        // create scanner object to take input
-        Scanner sc = new Scanner(System.in);
+    // create scanner object to take input
+    Scanner sc = new Scanner(System.in);
+    char choice;
+    do{
         float a = 0;
         float b = 0;
-        char choice;
         System.out.print("--------------------------------------------");
         System.out.print("\nChavvi Calc");
         System.out.print("\n--------------------------------------------");
@@ -41,15 +63,17 @@ public class ChavviCalculator
         if (choice != 'a' && choice != 'b' && choice != '+' && choice != '-' && choice != '*' && choice != '/' && choice != 'c' && choice != 'q') {
             System.out.println("ERROR: Unknown command");
         }
+
+        else if (choice == 'q') {
+            System.out.println("Thank you for using Chavvi Calculator");
+		}
+    } while (choice != 'q');
     }
 
-
-
     // main function
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         menu();
-        System.out.println( "Hello World!" );
     }
 
 }
