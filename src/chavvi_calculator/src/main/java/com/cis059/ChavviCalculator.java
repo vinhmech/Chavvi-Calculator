e484814f5435ae59d8a0693937d79ab0e7ca7498
vinhmech <vincenguyen262@gmail.com>
2021-08-28 15:10:16 -0700

    debug Chavvi Calculator

--- src/chavvi_calculator/src/main/java/com/cis059/ChavviCalculator.java
+++ src/chavvi_calculator/src/main/java/com/cis059/ChavviCalculator.java
@@ -14,28 +14,12 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class ChavviCalculator 
 {
-    // function to check if input is float 
-    public static boolean isFloat(String value) {
-        int decimalPoint = 0;
-        if ((value.charAt(0) < '0' || value.charAt(0) > '9') && value.charAt(0) != '.' && value.charAt(0) != '-') return false;
-        if (value.charAt(0) == '.' && value.charAt(1) == '-') return false;
-        for (int i = 1; i < value.length(); i++) {
-            if ((value.charAt(i) < '0' || value.charAt(i) > '9') && value.charAt(i) != '.') {
-                return false;
-            }
-            if (value.charAt(i) == '.') {
-                decimalPoint++;
-                if (decimalPoint > 1) return false;
-            }
-        }
-        return true;
-    }
 
     // print menu function
     public static void menu(){
     // create scanner object to take input
     Scanner sc = new Scanner(System.in);
-    char choice;
+    String choice = "";
     float a = 0;
     float b = 0;
     do{
@@ -54,57 +38,64 @@ public class ChavviCalculator
         System.out.print("\nq      Quit");
         System.out.print("\n--------------------------------------------");
         System.out.print("\nEnter a command: ");
-        choice = sc.next().charAt(0);  
-        // check if user input is valid
-        if (choice != 'a' && choice != 'b' && choice != '+' && choice != '-' && choice != '*' && choice != '/' && choice != 'c' && choice != 'q') {
+        choice = sc.nextLine();  
+        
+        // check if input is a valid char type
+        if (choice.length() != 1){
+            System.out.println("ERROR: Unknown command");
+        }
+        // check if user input is valid character
+        else if (choice.charAt(0) != 'a' && choice.charAt(0) != 'b' && choice.charAt(0) != '+' && choice.charAt(0) != '-' && choice.charAt(0) != '*' && choice.charAt(0) != '/' && choice.charAt(0) != 'c' && choice.charAt(0) != 'q') {
             System.out.println("ERROR: Unknown command");
         }
 
         // input a option
-        else if (choice == 'a') {
-            sc.nextLine();
+        // input a option
+        else if (choice.charAt(0) == 'a') {
+            //sc.nextLine();
             System.out.print("Enter a number: ");
-			String a_string;
-            a_string = sc.nextLine();
-			if (isFloat(a_string) == true) {
-                a = Float.parseFloat(a_string);
-			}
-			else {
-                System.out.println("ERROR: The value entered is not a floating point number");
-			}
-		}
+			      String a_string;
+            try {
+              a_string = sc.nextLine();
+              a = Float.parseFloat(a_string);
+            }
+            catch (NumberFormatException e) {
+              System.out.println("ERROR: The value entered is not a floating point number");
+            }
+        }
 
         // input b option
-        else if (choice == 'b') {
-            sc.nextLine();
+        // input b option
+        else if (choice.charAt(0) == 'b') {
+            //sc.nextLine();
             System.out.print("Enter a number: ");
-			String b_string;
-            b_string = sc.nextLine();
-			if (isFloat(b_string) == true) {
-                b = Float.parseFloat(b_string);
-			}
-			else {
-                System.out.println("ERROR: The value entered is not a floating point number");
-			}
+			      String b_string;
+            try{
+              b_string = sc.nextLine();
+              b = Float.parseFloat(b_string);
+            }
+            catch (NumberFormatException e){
+              System.out.println("ERROR: The value entered is not a floating point number");
+            }
 		}
 
         // + option
-		else if (choice == '+') {
+		else if (choice.charAt(0) == '+') {
 			a = a + b;
 		}
 
         // - option
-		else if (choice == '-') {
+		else if (choice.charAt(0) == '-') {
 			a = a - b;
 		}
         
         // * option
-        else if (choice == '*') {
+        else if (choice.charAt(0) == '*') {
 			a = a * b;
 		}
 
         // / option
-        else if (choice == '/') {
+        else if (choice.charAt(0) == '/') {
 			if (b == 0) {
                 System.out.println("ERROR: Unable to divide by 0");
 			}
@@ -114,17 +105,17 @@ public class ChavviCalculator
 		}
 
         // clear option 
-        else if (choice == 'c') {
+        else if (choice.charAt(0) == 'c') {
 			a = 0;
 			b = 0;
 		}
 
         // quit option
-        else if (choice == 'q') {
+        else if (choice.charAt(0) == 'q') {
             sc.close();
             System.out.println("Thank you for using Chavvi Calculator");
 		}
-    } while (choice != 'q');
+    } while (choice.equals("") || choice.charAt(0) != 'q');
     }
 
     // main function
